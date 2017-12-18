f009ab95b17d913d99161daaf41df89021d17b8d
Sushil George <sgk.george@gmail.com>
2017-12-19 01:32:59 +0530

    testing with single branch

--- Assets/Scripts/NewBehaviourScript1.cs
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Some random comments
 public class NewBehaviourScript1 : MonoBehaviour
 {
 
   // Use this for initialization
   void Start ()
   {
-    // hahaha!
+    // hahaha! This is another conflict!
+    // New line added
   }
 
   // Update is called once per frame
